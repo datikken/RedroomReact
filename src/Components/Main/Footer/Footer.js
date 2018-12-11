7300c620a0601c248c3c1f94a9da54e16f5374a9
Tikken <tikken23@gmail.com>
2018-12-11 18:15:33 +0200

    socialNetworkicons

--- src/Components/Main/Footer/Footer.js
+++ src/Components/Main/Footer/Footer.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faVk
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
@@ -7,11 +13,14 @@ const Footer = () => {
       <div class="row">
         <div class="col l6 s12">
           <h5 class="white-text">Мы в соц. сетях:</h5>
+          <a href="https://vk.com/datikken" target="blank"><FontAwesomeIcon icon={faVk} inverse /></a>
+          <a href="https://www.instagram.com/tikkentikken/" target="blank"><FontAwesomeIcon icon={faInstagram} inverse /></a>
+          <a href="https://www.facebook.com/tikken.tikken.3" target="blank"><FontAwesomeIcon icon={faFacebook} inverse /></a>
           </div>
         <div class="col l4 offset-l2 s12">
           <h5 class="white-text">Ссылки:</h5>
           <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">S</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">1 2 3</a></li>
           </ul>
         </div>
       </div>
